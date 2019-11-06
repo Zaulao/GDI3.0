806feb3d7dbabf81e060b5848060b6953be264cf
Saulo Guilhermino Ferreira Lima <sgfl@ug3c11.windows.cin.ufpe.br>
2019-11-06 09:36:59 -0300

    ainda criando tipos OR

--- SQL/criacao_tipos.sql
+++ SQL/criacao_tipos.sql
@@ -1,3 +1,21 @@
+CREATE OR REPLACE TYPE tp_pessoa AS OBJECT(
+    nome VARCHAR2(255),
+    ID NUMBER,
+    email VARCHAR2(255),
+    MEMBER FUNCTION comparaID (p tp_pessoa) RETURN INTEGER
+)NOT FINAL;
+/
+
+CREATE OR REPLACE TYPE BODY tp_pessoa AS
+MEMBER FUNCTION comparaID (p tp_pessoa) RETURN INTEGER IS
+RETURN      
+      CASE
+            WHEN SELF.ID = P.ID then 0
+            WHEN SELF.ID <> P.ID then 1            
+            END;
+      END;
+/       
+
 CREATE OR REPLACE TYPE tp_fone AS OBJECT(
     NUMERO NUMBER
 );
@@ -6,9 +24,49 @@ CREATE OR REPLACE TYPE tp_fone AS OBJECT(
 CREATE OR REPLACE TYPE tp_nt_fone AS TABLE OF tp_fone;
 /
 
-create OR REPLACE TYPE tp_pessoa AS OBJECT(
-    nome VARCHAR2(255) NOT NULL,
-    ID NUMBER NOT NULL,
-    email VARCHAR2(255),
-)NOT FINAL;
+CREATE OR REPLACE TYPE tp_contato AS OBJECT(
+    site VARCHAR2(255),
+    fones tp_nt_fone
+);
+/
+
+CREATE OR REPLACE TYPE tp_artista UNDER tp_pessoa(
+    contatos tp_contato
+);
+/
+
+CREATE OR REPLACE TYPE tp_usuario UNDER tp_pessoa(
+    idade NUMBER
+);
+/
+
+CREATE OR REPLACE TYPE tp_genero AS OBJECT(
+    genero VARCHAR(255)
+);
+/
+
+CREATE OR REPLACE TYPE tp_generos AS VARRAY(5) OF tp_genero;
+/
+
+CREATE OR REPLACE TYPE tp_musica AS OBJECT(
+    ID NUMBER,
+    nome VARCHAR(255), 
+    duracao_segundos NUMBER,
+    l_generos tp_generos,
+    FINAL MAP MEMBER FUNCTION musicaToInt RETURN VARCHAR2,
+    ORDER MEMBER FUNCTION comparaDuracao (X tp_musica) RETURN INTEGER
+);
+/
+
+CREATE OR REPLACE TYPE BODY tp_musica AS
+FINAL MAP MEMBER FUNCTION musicaToInt RETURN NUMBER IS
+    p NUMBER:= ID;
+    begin
+      RETURN p;
+    end;
+ORDER MEMBER FUNCTION comparaDuracao (X tp_musica) RETURN NUMBER IS
+begin
+  RETURN SELF.duracao_segundos - X.duracao_segundos;
+end;
+END;
 /
