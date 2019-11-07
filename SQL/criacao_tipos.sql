2062dcf33d647ae64f635a6b5ff2a2edb6a30adf
Saulo Guilhermino Ferreira Lima <sgfl@ug4c12.windows.cin.ufpe.br>
2019-11-07 08:51:54 -0300

    tipos criados mas não testados

--- SQL/criacao_tipos.sql
+++ SQL/criacao_tipos.sql
@@ -31,18 +31,45 @@ CREATE OR REPLACE TYPE tp_contato AS OBJECT(
 /
 
 CREATE OR REPLACE TYPE tp_artista UNDER tp_pessoa(
+    CONSTRUCTOR FUNCTION tp_artista (x1 tp_pessoa)
+        RETURN SELF AS RESULT,
     contatos tp_contato
 );
 /
 
 CREATE OR REPLACE TYPE tp_usuario UNDER tp_pessoa(
+    CONSTRUCTOR FUNCTION tp_artista (x1 tp_pessoa)
+        RETURN SELF AS RESULT,
     idade NUMBER
 );
+/
+
+CREATE OR REPLACE TYPE BODY tp_usuario AS
+OVERRIDING MEMBER FUNCTION comparaID (p tp_pessoa)  RETURN INTEGER IS
+   m tp_usuario;
+      r integer;
+     begin
+        if p is of ( tp_usuario ) then
+           m := treat( p as tp_usuario );
+           r :=
+             case
+                when self.ID = m.ID then 0
+                 when self.ID > m.ID then 1
+                when self.ID < m.ID then -1
+               end;
+       end if;
+   return r;
+     end;
+end;
 /
 
 CREATE OR REPLACE TYPE tp_genero AS OBJECT(
     genero VARCHAR(255)
 );
+/
+CREATE OR REPLACE TYPE tp_genero2 AS OBJECT(
+    genero VARCHAR(244)
+)NOT INSTANTIABLE;
 /
 
 CREATE OR REPLACE TYPE tp_generos AS VARRAY(5) OF tp_genero;
@@ -57,7 +84,7 @@ CREATE OR REPLACE TYPE tp_musica AS OBJECT(
 );
 /
 
-ALTER TYPE tp_musica ADD ATTRIBYTE (duracao_segundos NUMBER) CASCADE;
+ALTER TYPE tp_musica ADD ATTRIBUTE (duracao_segundos NUMBER) CASCADE;
 /
 
 CREATE OR REPLACE TYPE BODY tp_musica AS
@@ -73,11 +100,32 @@ end;
 END;
 /
 
---INSERT table1 (approvaldate) VALUES (CONVERT(datetime,'18-06-12 10:34:09 PM',5));
+CREATE OR REPLACE TYPE tp_nt_musica AS TABLE OF tp_musica;
+/
+
+--INSERT table1 (approvaldate) VALUES (CONVERT(date,'18-06-12', 5));
 CREATE OR REPLACE TYPE tp_album AS OBJECT(
     ID NUMBER,
     nome VARCHAR2(255),
     data_lancamento DATE,
-
+    musicas tp_nt_musica
 );
+/
+
+CREATE OR REPLACE TYPE tp_playlist AS OBJECT(
+    nome VARCHAR2(255),
+    musicas tp_nt_musica,
+    MEMBER PROCEDURE alteraNome(SELF tp_playlist)
+);
+/
+
+CREATE OR REPLACE TYPE BODY tp_playlist AS
+    MEMBER PROCEDURE alteraNome(SELF tp_playlist, novo_nome VARCHAR2(255)) IS
+        BEGIN
+            SELF.nome := novo_nome;
+        END;
+END;
+/
+
+ALTER TYPE tp_usuario ADD ATTRIBUTE (playlist tp_playlist) CASCADE;
 /
